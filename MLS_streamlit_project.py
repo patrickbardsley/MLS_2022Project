d808562e488deb8db4f83f2b9af9ac108cc4886a
patrickbardsley <padl.bard@gmail.com>
2023-12-04 19:47:36 -0500

    Changed

--- MLS_streamlit_project.py
+++ MLS_streamlit_project.py
@@ -15,7 +15,7 @@ y_val = st.sidebar.selectbox("Pick your y-axis", df.select_dtypes(include=np.num
 scatter = alt.Chart(df, title=f"{x_val} and {y_val}").mark_point().encode(
     alt.X(x_val,title=f"{x_val}"),
     alt.Y(y_val,title=f"{y_val}"),
-    opacity = 0.5,
+    #opacity = 0.5,
     tooltip=[x_val,y_val]).configure(background='#D9E9F0')
 st.altair_chart(scatter, use_container_width=True)
 
