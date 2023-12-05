72f1e72da1c6c1ad77ddc8ffbf5272f65346fb67
patrickbardsley <padl.bard@gmail.com>
2023-12-04 19:42:31 -0500

    Opacity to scatter plot

--- MLS_streamlit_project.py
+++ MLS_streamlit_project.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
+import seaborn as sns
 
 st.title('2022 MLS Season Visualisations')
 
@@ -11,9 +12,10 @@ df = pd.read_csv('MLS_performance_data_22_experiment.csv', encoding='latin-1')
 x_val = st.sidebar.selectbox("Pick your x-axis", df.select_dtypes(include=np.number).columns.tolist())
 y_val = st.sidebar.selectbox("Pick your y-axis", df.select_dtypes(include=np.number).columns.tolist())
 
-scatter = alt.Chart(df, title=f"Correlation between {x_val} and {y_val}").mark_point().encode(
+scatter = alt.Chart(df, title=f"{x_val} and {y_val}").mark_point().encode(
     alt.X(x_val,title=f"{x_val}"),
     alt.Y(y_val,title=f"{y_val}"),
+    opacity = 0.5,
     tooltip=[x_val,y_val]).configure(background='#D9E9F0')
 st.altair_chart(scatter, use_container_width=True)
 
